1d4a44f4b4ea49843ad674e3185a7db77af6a6f7
AliaMajeed <aliamajeed2402@gmail.com>
2025-04-24 11:26:30 +0500

    Arrays & Dictionary

---
+++ Data_Structure_In_Python.py
@@ -0,0 +1,71 @@
+# Topics Covered
+# Array: - Array of array - Array of dicts - Array of  tuples 
+#1....Array (list) of Arrays
+# A list containing other lists.
+array_of_arrays= [
+    [1,2,3,4],
+    [3,4,5,6],
+    [3,9,8,7]
+]
+print("Array of Arrays:")
+print(array_of_arrays)
+print()
+
+#2.... Array (list) of Dictionaries
+# A list where each item is a dictionary.
+array_of_dicts = [
+    {"name": "Alice", "age": 25},
+    {"name": "Bob", "age": 30}
+]
+print("Array of Dicts:")
+print(array_of_dicts)
+print()
+
+
+#3.....  Array (list) of Tuples
+# A list where each item is a tuple.
+array_of_tuples = [(1, 'a'), (2, 'b'), (3, 'c')]
+print("Array of Tuples:")
+print(array_of_tuples)
+print()
+
+#  Dictionary: - Array as values - Tuple as 
+# values - Dictionary as values
+#4.... Dictionary with Arrays (lists) as Values
+# Each key maps to a list.
+dict_with_arrays = {
+    "fruits": ["apple", "banana", "mango"],
+    "vegetables": ["carrot", "spinach"]
+}
+print("Dictionary with Arrays as Values:")
+print(dict_with_arrays)
+print()
+
+#5....  Dictionary with Tuples as Values
+# Each key maps to a tuple.
+dict_with_tuples = {
+    "point1": (2, 3),
+    "point2": (5, 7)
+}
+print("Dictionary with Tuples as Values:")
+print(dict_with_tuples)
+print()
+
+#6.....  Dictionary with Dictionaries as Values
+# Each key maps to another dictionary.
+dict_with_dicts = {
+    "person1": {"name": "Alice", "age": 25},
+    "person2": {"name": "Bob", "age": 30}
+}
+print("Dictionary with Dictionaries as Values:")
+print(dict_with_dicts)
+print()
+
+
+
+
+
+
+
+
+
